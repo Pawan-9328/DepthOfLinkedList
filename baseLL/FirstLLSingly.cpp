6a9e3c97ca4def3ead64dfb9cf473b0b6fe40357
Pawan Gola <pawangola908@gmail.com>
2024-03-06 16:42:19 +0530

    middle part add

--- baseLL/FirstLLSingly.cpp
+++ baseLL/FirstLLSingly.cpp
@@ -25,11 +25,39 @@ void InsertAtHead(Node *&head, int d)
 }
 
 void InsertAtTail(Node *&tail, int d)
-{    
-   // new node create 
+{
+   // new node create
    Node *temp = new Node(d);
    tail->next = temp;
    tail = temp;
+}
+
+void InsertAtPosition(Node *&head, Node *&tail, int position, int d)
+{
+   if (position == 1)
+   {
+      InsertAtHead(head, d);
+      return;
+   }
+
+   Node *temp = head;
+   int cnt = 1;
+   while (cnt < position - 1)
+   {
+      temp = temp->next;
+      cnt++;
+   }
+   // inserting at last position
+   if (temp->next == NULL)
+   {
+      InsertAtTail(tail, d);
+      return;
+   }
+
+   // creating a node for d
+   Node *nodeToInsert = new Node(d);
+   nodeToInsert->next = temp->next;
+   temp->next = nodeToInsert;
 }
 
 void print(Node *&head)
@@ -57,7 +85,12 @@ int main()
    InsertAtTail(tail, 12);
    print(head);
 
-   InsertAtTail(tail, 22);
+   InsertAtTail(tail, 15);
    print(head);
+   InsertAtPosition(head, tail, 4, 22);
+   print(head);
+
+   cout<<"Head "<< head -> data<<endl;
+   cout<<"Tail "<< tail -> data<<endl;
    return 0;
 }
